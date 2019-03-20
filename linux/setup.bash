332c75dec6e70b1a0989b5276c9b606b12f8c420
Jmac <jonathan.macdonald@theinformationlab.co.uk>
2019-03-20 11:14:45 +0000

    Removed hard-coded variables

--- linux/setup.bash
+++ linux/setup.bash
@@ -7,6 +7,9 @@
   }
 
 # Loading necessary environment
+source /etc/profile.d/tableau_server.sh
+
+# In case that fails, then we'll try this
 load_environment_file() {
   if [[ -f /etc/opt/tableau/tableau_server/environment.bash ]]; then
     source /etc/opt/tableau/tableau_server/environment.bash
@@ -14,13 +17,11 @@ load_environment_file() {
   fi
 }
 
-source /etc/profile.d/tableau_server.sh
-
 # Some more variables
-script_url=https://raw.githubusercontent.com/til-jmac/tableau-server-housekeeping/master/linux/tableau-server-housekeeping-linux.bash
+script_url='https://raw.githubusercontent.com/til-jmac/tableau-server-housekeeping/master/linux/tableau-server-housekeeping-linux.bash'
 script_file='tableau-server-housekeeping-linux.bash'
-script_dir=/var/opt/tableau/tableau_server/scripts
-tmp_dir=/tmp
+script_dir='$TABLEAU_SERVER_DATA_DIR/scripts'
+tmp_dir='/tmp'
 
 # Setting up housekeeping script
 cd $tmp_dir
@@ -31,7 +32,7 @@ if [[ ! -e $script_dir ]]; then
 fi
 
 cp $script_file $script_dir
-chown -R tableau:tableau $script_dir
+chown -R $TABLEAU_SERVER_UNPRIVILEGED_USERNAME:$TABLEAU_SERVER_UNPRIVILEGED_USERNAME $script_dir
 chmod +x $script_dir/$script_file
 
 # Script is set up, now to schedule in crontab
