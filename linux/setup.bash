b20e2e4cbbb3dfc6666e28952a06012723b0d05e
Jmac <jonathan.macdonald@theinformationlab.co.uk>
2019-12-16 13:15:24 +0000

    Updated to pull separate backup and cleanup scripts instead

--- linux/setup.bash
+++ linux/setup.bash
@@ -18,22 +18,28 @@ load_environment_file() {
 }
 
 # Some more variables
-script_url='https://raw.githubusercontent.com/til-jmac/tableau-server-housekeeping/master/linux/tableau-server-housekeeping-linux.bash'
-script_file='tableau-server-housekeeping-linux.bash'
+backup_script_url='https://raw.githubusercontent.com/til-jmac/tableau-server-housekeeping/master/linux/tableau-server-backup.bash'
+backup_script_file='tableau-server-backup.bash'
+cleanup_script_url='https://raw.githubusercontent.com/til-jmac/tableau-server-housekeeping/master/linux/tableau-server-logs-cleanup.bash'
+cleanup_script_file='tableau-server-logs-cleanup.bash'
+
 script_dir="${TABLEAU_SERVER_DATA_DIR}/scripts"
 tmp_dir='/tmp'
 
 # Setting up housekeeping script
 cd $tmp_dir
-wget $script_url
+wget $backup_script_url
+wget $cleanup_script_url
 
 if [[ ! -e $script_dir ]]; then
     mkdir $script_dir
 fi
 
-cp $script_file $script_dir
+cp $backup_script_file $script_dir
+cp $cleanup_script_file $script_dir
 chown -R $TABLEAU_SERVER_UNPRIVILEGED_USERNAME:$TABLEAU_SERVER_UNPRIVILEGED_USERNAME $script_dir
-chmod +x $script_dir/$script_file
+chmod +x $script_dir/$backup_script_file
+chmod +x $script_dir/$cleanup_script_file
 
 # Script is set up, now to schedule in crontab
 echo 'Tableau Server housekeeping script is now installed. Please refer to the script documentation to schedule in crontab'
